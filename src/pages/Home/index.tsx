defda3ba947c8087d42a9ae51603fcafb49b3c86
Roberto Modenezi Neto <futspamslt@gmail.com>
2023-06-21 22:49:02 -0300

    Refect code Javascript to Typescript

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,28 @@
 import { useState, useEffect } from 'react'
 
-import { Card } from "../../components/Card"
+import { Card, ICardProps } from "../../components/Card"
 
 import "./style.css"
 
+interface IProfileResponse {
+  name: string;
+  avatar_url: string;
+}
+
+interface IUser {
+  name: string;
+  avatar: string;
+}
+
 export function Home() {
-  const [user, setUser] = useState({ name: "", avatar: ""})
-  const [studentName, setStudentName] = useState("") ;
-  const [students, setStudents] = useState ([])
+  const [user, setUser] = useState<IUser>({} as IUser)
+  const [studentName, setStudentName] = useState<string>("") ;
+  const [students, setStudents] = useState<ICardProps []> ([])
 
   useEffect(() => {
    async function fetchData() {
-      const response = await fetch("https://api.github.com/users/RobertoModeneziNeto")
-      const data = await response.json()
+      const response = await fetch("https://api.github.com/users/RobertoModeneziNeto");
+      const data = await response.json() as IProfileResponse;
       setUser({
         name: data.name,
         avatar: data.avatar_url
@@ -25,10 +35,8 @@ export function Home() {
     
 
   function handleAddStudent() {
-    if (!studentName) {
-      alert("O nome precisa ser preenchido!")
-      return;
-    }
+    
+
     const newStudent = {
       name: studentName,
       time: new Date().toLocaleTimeString("pt-br", {
@@ -38,7 +46,7 @@ export function Home() {
       })
     }
 
-    setStudents([...students, newStudent])
+    setStudents((prevState) => [...prevState, newStudent])
   }
 
   return (
@@ -57,7 +65,12 @@ export function Home() {
         placeholder="Digite um nome ..."
         onChange={(e) => {setStudentName(e.target.value) }}   
       />
-      <button onClick={handleAddStudent}>Adicionar</button>
+      <button 
+        disabled = {!studentName}
+        onClick={handleAddStudent}
+       >
+        Adicionar
+      </button>
 
       {students.map((student) => (
         <Card key={student.time} name = {student.name} time = {student.time} />
